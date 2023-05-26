cf6efbede4704100b6e0897d29b1fda9db53257e
irenepintor <pintor.irene@outlook.com>
2023-05-26 20:02:23 -0300

    Adding navbar items-Styling site-Product card JS

---
+++ assets/src/pages-src/product-card.js
@@ -0,0 +1,42 @@
+{/* <section id="product-box">
+    Generar desde DOM las tarjetas de producto
+    <div>
+        <ul>
+            <li>
+                <a href="">
+                    <img src="" alt="">
+                    <h3>Articulo Nro. # - Nombre de Producto</h3>
+                </a>
+                <div class="color-selector">
+                    <div class="pellet-container">
+                        <a href=""></a>
+                    </div>
+                    <div class="pellet-container">
+                        <a href=""></a>
+                    </div>
+                    <div class="pellet-container">
+                        <a href=""></a>
+                    </div>
+                    <div class="pellet-container">
+                        <a href=""></a>
+                    </div>
+                    O más pellets según la prenda.
+                </div>
+                <a href="" class="">
+                    <h3>Articulo Nro. # - Nombre de Producto</h3>
+                    <span class="price">
+                        <span>Precio</span>
+                        <span>+ IVA</span>
+                    </span>
+                    <div class="">
+                        Cuotas
+                        <span>Precio de la Cuota</span>
+                        ,
+                        <span>Centavos - De ser necesario</span>
+                        sin Interés
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
+</section> */}
