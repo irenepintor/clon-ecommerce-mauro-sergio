ce437127123861a5bb349c82836118070ac471d9
irenepintor <pintor.irene@outlook.com>
2023-05-31 20:03:13 -0300

    Simple age request process to be able to enter the site or not.

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,25 +1,25 @@
-// const edad = 18; 
-//     let ingreso = parseInt(prompt ("Ingrese su edad.")); 
-//     let repetir = true; 
+const edad = 18; 
+    let ingreso = parseInt(prompt ("Ingrese su edad.")); 
+    let repetir = true; 
 
-// function coincide (ingreso) { 
-//     return (18 <= ingreso) 
-// }; 
+function coincide (ingreso) { 
+    return (18 <= ingreso) 
+}; 
 
-// if (coincide(ingreso)) { 
-//     alert ("Listo. Ya puedes comprar!"); 
-//     repetir = false; 
-// } 
+if (coincide(ingreso)) { 
+    alert ("Listo. Ya puedes comprar!"); 
+    repetir = false; 
+} 
 
-// while (repetir){ 
-//     if(isNaN(ingreso)){ 
-//         ingreso = parseInt(prompt("Por favor, ingrese un número válido!❌")); 
-//         continue 
-//     }else if(coincide(ingreso)) { 
-//         alert ("Listo. Ya puedes comprar!"); 
-//         repetir = false; 
-//     }else{  
-//         alert ("Eres menor, debes hablar con tus mayores."); 
-//         repetir = false; 
-//     } 
-// }
+while (repetir){ 
+    if(isNaN(ingreso)){ 
+        ingreso = parseInt(prompt("Por favor, ingrese un número válido!❌")); 
+        continue 
+    }else if(coincide(ingreso)) { 
+        alert ("Listo. Ya puedes comprar!"); 
+        repetir = false; 
+    }else{  
+        alert ("Eres menor, debes hablar con tus mayores."); 
+        repetir = false; 
+    } 
+}
