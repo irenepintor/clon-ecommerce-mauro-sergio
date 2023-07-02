cf7fb1d222953ce8b77a04843c22086e8ea9a2a6
irenepintor <pintor.irene@outlook.com>
2023-07-02 20:43:58 -0300

    cart display fixed | checkbox filter fixing

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -36,25 +36,31 @@ function addProduct(id) {
     }
     saveCartLS(cart);
     cartButton();
+    cartCards();
 }
 
 function deleteProduct(id) {
     const cart = getCartLS();
     let position = cart.findIndex((item) => item.id === id);
-    if (cart[position].amount > 1) {
+
+    if (position !== -1) {
+        if (cart[position].amount > 1) {
         cart[position].amount -= 1;
-    } else {
+        } else {
         cart.splice(position, 1);
+        }
+        saveCartLS(cart);
+        cartButton();
+        cartCards();
     }
-    //const newCart = cart.filter(item => item.id != id);
-    saveCartLS(cart);
-    cartButton();
-    productCards();
 }
 
 function emptyCart() {
     localStorage.removeItem('cart');
+    cartCards();
+    cartButton();
 }
+
 
 function totalProductsCart() {
     const cart = getCartLS();
@@ -118,54 +124,99 @@ function cartButton() {
 //     document.getElementById('productCards').innerHTML = cardContent;
 // }
 
-// function checkButtonFilter() {
-//     let allPoducts = getProductsLS();
-//     let check01 = document.getElementById('check01');
-//     let check02 = document.getElementById('check02');
-//     let check03 = document.getElementById('check03');
-//     let check04 = document.getElementById('check04');
-//     let check05 = document.getElementById('check05');
-//     let check06 = document.getElementById('check06');
-//     let check07 = document.getElementById('check07');
-//     let check08 = document.getElementById('check08');
-//     let content = '';
+function checkButtonFilter() {
+    let allPoducts = getProductsLS();
+    let check01 = document.getElementById('check01');
+    let check02 = document.getElementById('check02');
+    let check03 = document.getElementById('check03');
+    let check04 = document.getElementById('check04');
+    let check05 = document.getElementById('check05');
+    let check06 = document.getElementById('check06');
+    let check07 = document.getElementById('check07');
+    let check08 = document.getElementById('check08');
+    let content = '';
 
-//     //console.log((check01.checked && item.type == check01.value))
-
-//     allPoducts = allPoducts.filter(item => (check01.checked && item.type == check01.value) || (check02.checked && item.type == check02.value) || (check03.checked && item.type == check03.value) || (check04.checked && item.type == check04.value) || (check05.checked && item.type == check05.value) || (check06.checked && item.type == check06.value))
-
-//     if (check07.checked || check08.checked){
-//         allPoducts = allPoducts.filter(item => (check07.checked && item.gender == check07.value) || (check08.checked && item.gender == check08.value));
-//     }
-//     if (allPoducts.lenght > 0){
-//         allPoducts.forEach(allProduct =>{
-//             cardContent += `
-//             <div class="card-container mt-4 mb-4">
-//                 <div class="card">
-//                     <div class="card-body">
-//                     <li class="product-card">
-//                         <a href="categoria.html" onclick="seeProduct(${allProduct.id})">
-//                         <img src="${allProduct.img[0]}" alt="${allProduct.name}" class="img-fluid mb-2">
-//                         </a>
-//                         <div class="pellets">
-//                             <div>
-//                                 <a></a>
-//                             </div>
-//                         </div>
-//                         <div class="">
-//                             <a href="categoria.html" onclick="seeProduct(${allProduct.id})">
-//                                 <h5 class="card-title fs-6 cuotas">Art. ${allProduct.id} - ${allProduct.name}</h5>
-//                                 <span class="price fs-4 cuotas">$${allProduct.price}</span>
-//                             </a>
-//                         </div>
-//                     </li>
-//                     </div>
-//                 </div>
-//             </div>`;
-//             });
-//         document.getElementById('productCards').innerHTML = content;
-
-//     } else {
-//         productCards()
-//     }
-// }
+    if (!check01.checked &&
+        !check02.checked &&
+        !check03.checked &&
+        !check04.checked &&
+        !check05.checked &&
+        !check06.checked &&
+        !check07.checked &&
+        !check08.checked) {
+            displayAllProducts();
+            return;
+        }
+        allPoducts = allPoducts.filter(item =>
+            (check01.checked && item.type == check01.value) ||
+            (check02.checked && item.type == check02.value) ||
+            (check03.checked && item.type == check03.value) ||
+            (check04.checked && item.type == check04.value) ||
+            (check05.checked && item.type == check05.value) ||
+            (check06.checked && item.type == check06.value));
+    if (check07.checked || check08.checked) {
+        allPoducts = allPoducts.filter(item =>
+        (check07.checked && item.gender == check07.value) ||
+        (check08.checked && item.gender == check08.value));
+    }
+    if (allPoducts.length > 0) {
+        allPoducts.forEach(allProduct => {
+            content += `
+        <div class="card-container mt-4 mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <li class="product-card">
+                        <a href="categoria.html" onclick="seeProduct(${allProduct.id})">
+                            <img src="${allProduct.img[0]}" alt="${allProduct.name}" class="img-fluid mb-2">
+                        </a>
+                    <div class="pellets">
+                    <div>
+                        <a></a>
+                    </div>
+                </div>
+                <div class="">
+                    <a href="categoria.html" onclick="seeProduct(${allProduct.id})">
+                        <h5 class="card-title fs-6 cuotas">Art. ${allProduct.id} - ${allProduct.type} ${allProduct.name}</h5>
+                        <span class="price fs-4 cuotas">$${allProduct.price}</span>
+                    </a>
+                </div>
+                    </li>
+                </div>
+            </div>
+        </div>`;
+        });
+        document.getElementById('productCards').innerHTML = content;
+    } else {
+        document.getElementById('productCards').innerHTML = 'No se encontraron productos';
+    }
+}
+    function displayAllProducts() {
+    let allProducts = getProductsLS();
+    let content = '';
+    allProducts.forEach(allProduct => {
+    content += `
+        <div class="card-container mt-4 mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <li class="product-card">
+                    <a href="categoria.html" onclick="seeProduct(${allProduct.id})">
+                        <img src="${allProduct.img[0]}" alt="${allProduct.name}" class="img-fluid mb-2">
+                    </a>
+                    <div class="pellets">
+                        <div>
+                        <a></a>
+                        </div>
+                    </div>
+                    <div class="">
+                        <a href="categoria.html" onclick="seeProduct(${allProduct.id})">
+                        <h5 class="card-title fs-6 cuotas">Art. ${allProduct.id} - ${allProduct.type} ${allProduct.name}</h5>
+                        <span class="price fs-4 cuotas">$${allProduct.price}</span>
+                        </a>
+                    </div>
+                    </li>
+                </div>
+            </div>
+        </div>`;
+    });
+    document.getElementById('productCards').innerHTML = content;
+}
